ec0f625af28ca3b4e7dacdcd819b53a68ab858e6
Orurh <74089318+Orurh@users.noreply.github.com>
2023-12-17 19:56:38 +0300

    Initial commit

--- examples/decorator.py
+++ examples/decorator.py
@@ -1,3 +1,14 @@
+class Dec:
+    def __call__(self, func):
+        def wrapper (*args, **kwargs):
+            arg = [i*i for i in args]
+            res = func(*arg)
+            return res
+        return wrapper
 
-text = lambda: 'text'
-print(text())
+@Dec()
+def my_func(x1, x2):
+    return x1 + x2
+
+print(my_func(2,5))
+
